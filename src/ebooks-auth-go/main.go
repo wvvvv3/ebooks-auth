529547bcff3c8c2127bf929b867f62a6835011b1
wangyuwei <wangyuwei@bilibili.com>
2020-03-13 21:25:16 +0800

    feat:beego框架API版搭建

--- src/ebooks-auth-go/main.go
+++ src/ebooks-auth-go/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	_ "ebooks-auth-go/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
-
--- src/ebooks-auth-go/tests/default_test.go
+++ src/ebooks-auth-go/tests/default_test.go
@@ -13,23 +13,18 @@ import (
 )
 
 func init() {
-	// _, file, _, _ := runtime.Caller(1)
-	// apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
-	// beego.TestBeegoInit(apppath)
 	_, file, _, _ := runtime.Caller(1)
-	currpath, _ := os.Getwd()
-	currpath = filepath.Join(currpath, appname)
-	beego.TestBeegoInit(currpath)
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
+	beego.TestBeegoInit(apppath)
 }
 
-
-// TestBeego is a sample to run an endpoint test
-func TestBeego(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/", nil)
+// TestGet is a sample to run an endpoint test
+func TestGet(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/v1/object", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Trace("testing", "TestBeego", "Code[%d]\n%s", w.Code, w.Body.String())
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
 
 	Convey("Subject: Test Station Endpoint\n", t, func() {
 	        Convey("Status Code Should Be 200", func() {
